f78525a4a21b4f47bcba8e724c677c4ae4efbc3e
Paytonadean <padean@wpi.edu>
2021-11-01 17:48:55 -0400

    test

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+#include <sys/socket.h>
+class read_in{
+    ifstream file;
+    char* filename;
+    file.open(filename, ios::binary | ios::in))
+    //file.reAD(&)
+int front = socket(domain, type, protocol)
+std::thread obj()
+}
