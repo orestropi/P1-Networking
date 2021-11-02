5d1b2aa5bb73d453a04c25741af6506cbe383c7f
orestropi <oropi@wpi.edu>
2021-11-01 20:24:22 -0400

    Parsing of JPG QR code using system

--- test.cpp
+++ test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <thread>
 #include <sys/socket.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+//(part 1)
 class read_in{
     ifstream file;
     char* filename;
@@ -8,4 +12,17 @@ class read_in{
     //file.reAD(&)
 int front = socket(domain, type, protocol)
 std::thread obj()
+}
+
+//Decoding QR Code using Ssystem(part 2)
+int main ()
+{
+  int i;
+  printf ("Checking if processor is available...");
+  if (system(NULL)) puts ("Ok");
+    else exit (EXIT_FAILURE);
+  printf ("Parsing QR Code ...\n");
+  i=system ("java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner [image file]");
+  printf ("Parsed result: %d.\n",i);
+  return 0;
 }
