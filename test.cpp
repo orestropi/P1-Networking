8ce895717131c4aca50071843cc635a28dc3781e
orestropi <oropi@wpi.edu>
2021-11-01 20:42:51 -0400

    improved on QR decoder

--- test.cpp
+++ test.cpp
@@ -3,6 +3,22 @@
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+//Decoding QR Code using Ssystem(part 2)
+int qrDecode (string imageFile)
+{
+  int i;
+  printf ("Checking if processor is available...");
+  if (system(NULL)) puts ("Ok");
+    else exit (EXIT_FAILURE);
+  printf ("Parsing QR Code ...\n");
+  i=system ("java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner " + imageFile);
+  printf ("Parsed result: %d.\n",i);
+  return 0;
+}
 
 //(part 1)
 class read_in{
@@ -12,17 +28,4 @@ class read_in{
     //file.reAD(&)
 int front = socket(domain, type, protocol)
 std::thread obj()
-}
-
-//Decoding QR Code using Ssystem(part 2)
-int main ()
-{
-  int i;
-  printf ("Checking if processor is available...");
-  if (system(NULL)) puts ("Ok");
-    else exit (EXIT_FAILURE);
-  printf ("Parsing QR Code ...\n");
-  i=system ("java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner [image file]");
-  printf ("Parsed result: %d.\n",i);
-  return 0;
 }
