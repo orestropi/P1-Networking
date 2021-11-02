f20cf57d6a3760c1d3a57ddea5d8c9dde3f2f603
Paytonadean <padean@wpi.edu>
2021-11-01 20:28:04 -0400

    commit

--- test.cpp
+++ test.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <thread>
 #include <sys/socket.h>
+#include <fstream>
+
 class read_in{
-    ifstream file;
-    char* filename;
-    file.open(filename, ios::binary | ios::in))
-    //file.reAD(&)
-int front = socket(domain, type, protocol)
-std::thread obj()
+    char x;
+    std::ifstream file; //File being passed in
+    char* filename; //file name
+    uintmax_t size = file_size(filename);
+    //Function that reads in file
+    void read(std::ifstream file, char* filename)
+    {
+  
+    //opens file
+    file.open(filename, ios::binary | ios::in);
+    //reads in file based on size in bits
+    file.read(&x, sizeof(x));
+    file.seekp(0,ios::end);
+   // file
+    }
+    //makes a socket ??
+    //int front = socket(domain, type, protocol)
+    //Creates main thread and calls read function
+    thred th(read, file, filename);
 }
