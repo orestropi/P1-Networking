fb3258c4ee26aa0a64946956f58ad0c33c97b01c
orestropi <oropi@wpi.edu>
2021-11-01 21:03:20 -0400

    ...

--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <sys/socket.h>
+#include <fstream>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
@@ -20,12 +21,27 @@ int qrDecode (string imageFile)
   return 0;
 }
 
-//(part 1)
 class read_in{
-    ifstream file;
-    char* filename;
-    file.open(filename, ios::binary | ios::in))
-    //file.reAD(&)
-int front = socket(domain, type, protocol)
-std::thread obj()
+    char x;
+    std::ifstream file; //File being passed in
+    char* filename; //file name
+    uintmax_t size = file_size(filename);
+    //Function that reads in file
+    void read(std::ifstream file, char* filename)
+    {
+  
+    //opens file
+    file.open(filename, ios::binary | ios::in);
+    //reads in file based on size in bits
+    file.read(&x, sizeof(x));
+    file.seekp(0,ios::end);
+   // file
+    }
+    //makes a socket ??
+    //int front = socket(domain, type, protocol)
+    //Creates main thread and calls read function
+    thred th(read, file, filename);
 }
+
+
+
