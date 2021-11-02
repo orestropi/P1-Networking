57ed005d8de20c6fe3b8851bfc0dbbb2dce3230b
Paytonadean <padean@wpi.edu>
2021-11-01 20:56:36 -0400

    Read function

--- test.cpp
+++ test.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 #include <thread>
 #include <sys/socket.h>
-<<<<<<< HEAD
 #include <fstream>
+#include <sys/stat.h>
+#include <ios>
 
 class read_in{
     char x;
     std::ifstream file; //File being passed in
     char* filename; //file name
-    uintmax_t size = file_size(filename);
+    uintmax_t size = file.tellg();
     //Function that reads in file
     void read(std::ifstream file, char* filename)
     {
-  
-    //opens file
+        // Opening a file way 1
+        //std::ifstream myfile(filename, ios::in|ios::binary);
+    //opens file way 2
     file.open(filename, ios::binary | ios::in);
-    //reads in file based on size in bits
+    //ios is a class so I don't know why thats throwing an error
+    //reads in file based on size in bits -> Probably going to change to mach the ifstream
     file.read(&x, sizeof(x));
+    //Should keep track of position in file, hopefully based on bit position
     file.seekp(0,ios::end);
    // file
     }
@@ -25,30 +29,3 @@ class read_in{
     //Creates main thread and calls read function
     thred th(read, file, filename);
 }
-=======
-#include <stdlib.h>
-#include <stdio.h>
-
-//(part 1)
-class read_in{
-    ifstream file;
-    char* filename;
-    file.open(filename, ios::binary | ios::in))
-    //file.reAD(&)
-int front = socket(domain, type, protocol)
-std::thread obj()
-}
-
-//Decoding QR Code using Ssystem(part 2)
-int main ()
-{
-  int i;
-  printf ("Checking if processor is available...");
-  if (system(NULL)) puts ("Ok");
-    else exit (EXIT_FAILURE);
-  printf ("Parsing QR Code ...\n");
-  i=system ("java -cp javase.jar:core.jar com.google.zxing.client.j2se.CommandLineRunner [image file]");
-  printf ("Parsed result: %d.\n",i);
-  return 0;
-}
->>>>>>> 5d1b2aa5bb73d453a04c25741af6506cbe383c7f
