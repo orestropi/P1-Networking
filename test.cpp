7acb40fe8258b50f0809becbbea90abca34948d0
orestropi <oropi@wpi.edu>
2021-11-01 21:13:19 -0400

    ...

--- test.cpp
+++ test.cpp
@@ -2,6 +2,8 @@
 #include <thread>
 #include <sys/socket.h>
 #include <fstream>
+#include <sys/stat.h>
+#include <ios>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
@@ -25,15 +27,18 @@ class read_in{
     char x;
     std::ifstream file; //File being passed in
     char* filename; //file name
-    uintmax_t size = file_size(filename);
+    uintmax_t size = file.tellg();
     //Function that reads in file
     void read(std::ifstream file, char* filename)
     {
-  
-    //opens file
+        // Opening a file way 1
+        //std::ifstream myfile(filename, ios::in|ios::binary);
+    //opens file way 2
     file.open(filename, ios::binary | ios::in);
-    //reads in file based on size in bits
+    //ios is a class so I don't know why thats throwing an error
+    //reads in file based on size in bits -> Probably going to change to mach the ifstream
     file.read(&x, sizeof(x));
+    //Should keep track of position in file, hopefully based on bit position
     file.seekp(0,ios::end);
    // file
     }
