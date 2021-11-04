6f99e921827b1ba9863a2aeae2a2e532f310b4e2
Paytonadean <padean@wpi.edu>
2021-11-04 09:36:14 -0400

    Add server and client

---
+++ client.cpp
@@ -0,0 +1,19 @@
+//Client side
+#include <iostream>
+#include <thread>
+#include <sys/socket.h>
+#include <fstream>
+#include <sys/stat.h>
+#include <ios>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+int main(){
+    int clientSocket;
+    char buffer[1024];
+   // struct sock_addr_in serverAdd;
+    socklen_t address_size;
+    
+
+}
